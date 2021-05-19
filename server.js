ba483730a0cfa60180e09fbf8b9b15d9464a2bb3
Rachel <rmshinski@gmail.com>
2021-05-19 18:53:05 -0400

    homework

--- server.js
+++ server.js
@@ -1,27 +1,23 @@
 const http = require('http');
 const fs = require('fs');
+const _ = require('lodash');
 
 const server = http.createServer((req, res) => {
-  // console.log(req);
-  console.log(req.url);
 
+  // lodash
+  const num = _.random(0, 20);
+  console.log(num);
 
+  const greet = _.once(() => {
+    console.log('hello');
+  });
+  greet();
+  greet();
+
+  // set header content type
   res.setHeader('Content-Type', 'text/html');
 
-  // res.write('<p>hello, ninjas</p>');
-  // res.write('<p>hello again, ninjas</p>');
-  // res.end();
-
-
-  // fs.readFile('./views/index.html', (err, data) => {
-  //   if (err) {
-  //     console.log(err);
-  //     res.end();
-  //   }
-  //   //res.write(data);
-  //   res.end(data);
-  // });
-
+  // routing
   let path = './views/';
   switch(req.url) {
     case '/':
@@ -42,7 +38,7 @@ const server = http.createServer((req, res) => {
       res.statusCode = 404;
   }
 
-  
+  // send html
   fs.readFile(path, (err, data) => {
     if (err) {
       console.log(err);
@@ -55,7 +51,7 @@ const server = http.createServer((req, res) => {
 
 });
 
-
+// localhost is the default value for 2nd argument
 server.listen(3000, 'localhost', () => {
   console.log('listening for requests on port 3000');
 });
